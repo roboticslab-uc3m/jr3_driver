747a763a59d904275d5be90e40a7bdc801dcbc82
Bartek Łukawski <bwmn.peter@gmail.com>
2026-02-06 21:20:20 +0100

    Improve shutdown handling

--- jr3_driver/jr3_driver.py
+++ jr3_driver/jr3_driver.py
@@ -12,6 +12,8 @@ class Jr3Driver(Node):
     def __init__(self):
         super().__init__('jr3_driver')
 
+        self.context.on_shutdown(self.close)
+
         channel_param = self.declare_parameter('channel', '/dev/ttyLPC1768',
             ParameterDescriptor(description='Serial channel name'))
         baudrate_param = self.declare_parameter('baudrate', 115200,
@@ -33,57 +35,79 @@ class Jr3Driver(Node):
         jr3_deadband_torques_param = self.declare_parameter('jr3_deadband_torques', 0.0,
             ParameterDescriptor(description='JR3 deadband on torque measurements (N*m)'))
 
-        self.R_jr3_tcp = Rotation.RPY(jr3_roll_param.get_parameter_value().double_value,
+        self._R_jr3_tcp = Rotation.RPY(jr3_roll_param.get_parameter_value().double_value,
                                       jr3_pitch_param.get_parameter_value().double_value,
                                       jr3_yaw_param.get_parameter_value().double_value)
 
-        self.jr3_deadband_forces = jr3_deadband_forces_param.get_parameter_value().double_value
-        self.jr3_deadband_torques = jr3_deadband_torques_param.get_parameter_value().double_value
+        self._jr3_deadband_forces = jr3_deadband_forces_param.get_parameter_value().double_value
+        self._jr3_deadband_torques = jr3_deadband_torques_param.get_parameter_value().double_value
 
-        self.publisher = self.create_publisher(Wrench, 'jr3', 10)
+        self._publisher = self.create_publisher(Wrench, 'jr3', 10)
 
         self.jr3 = Jr3Manager(channel=channel_param.get_parameter_value().string_value,
                               baudrate=baudrate_param.get_parameter_value().integer_value)
 
+        self._timer = None
+
+        try:
+            self._setup_jr3(cutoff_param, read_period_param)
+
+            self._timer = self.create_timer(publish_period_param.get_parameter_value().double_value * 0.001,
+                                            self._timer_callback)
+
+            self.get_logger().info('JR3 sensor is running.')
+        except Exception as e:
+            self.get_logger().fatal(f'Initialization failed: {e}')
+            self.close()
+            raise e
+
+    def _setup_jr3(self, cutoff_param, read_period_param):
         ret, fs, state = self.jr3.get_fs()
 
+        if not ret or state != self.jr3._state.READY:
+            raise RuntimeError('Failed to get JR3 sensor full scale factors or sensor not ready')
+
         self.get_logger().info(f'JR3 sensor state: {state}, full scale: {fs}')
-
-        self.jr3.stop() # might be already running, ensure it's stopped
-
+        self.jr3.stop() # might be already running, so ensure it's stopped
         self.get_logger().info('Starting JR3 sensor...')
 
-        self.jr3.start(cutoff_freq=cutoff_param.get_parameter_value().double_value,
-                       period_ms=read_period_param.get_parameter_value().double_value)
+        ret, state = self.jr3.start(cutoff_freq=cutoff_param.get_parameter_value().double_value,
+                                    period_ms=read_period_param.get_parameter_value().double_value)
 
-        success, forces, torques, _ = self.jr3.read()
-        self.get_logger().info(f'Initial read success: {success}, forces: {forces}, torques: {torques}')
+        if not ret:
+            raise RuntimeError('Failed to start JR3 sensor')
 
         self.get_logger().info('Zeroing JR3 sensor...')
-
         time.sleep(1)
-        self.jr3.zero_offs()
+        ret, state = self.jr3.zero_offs()
 
-        self.get_logger().info('JR3 sensor is running.')
+        if not ret:
+            raise RuntimeError('Failed to zero JR3 sensor')
 
-        self.timer = self.create_timer(publish_period_param.get_parameter_value().double_value * 0.001,
-                                       self.timer_callback)
+    def close(self):
+        self.get_logger().info('Closing JR3 driver.')
 
-    def timer_callback(self):
+        if self._timer is not None:
+            self._timer.cancel()
+
+        if hasattr(self, 'jr3') and self.jr3 is not None:
+            self.jr3.close()
+
+    def _timer_callback(self):
         success, forces, torques, _ = self.jr3.read()
 
         if success:
             kdl_forces = Vector(forces[0], forces[1], forces[2])
             kdl_torques = Vector(torques[0], torques[1], torques[2])
 
-            if kdl_forces.Norm() < self.jr3_deadband_forces:
+            if kdl_forces.Norm() < self._jr3_deadband_forces:
                 kdl_forces = Vector.Zero()
 
-            if kdl_torques.Norm() < self.jr3_deadband_torques:
+            if kdl_torques.Norm() < self._jr3_deadband_torques:
                 kdl_torques = Vector.Zero()
 
-            kdl_forces = self.R_jr3_tcp * kdl_forces
-            kdl_torques = self.R_jr3_tcp * kdl_torques
+            kdl_forces = self._R_jr3_tcp * kdl_forces
+            kdl_torques = self._R_jr3_tcp * kdl_torques
 
             msg = Wrench()
             msg.force = Vector3(x=kdl_forces[0],
@@ -92,13 +116,28 @@ class Jr3Driver(Node):
             msg.torque = Vector3(x=kdl_torques[0],
                                  y=kdl_torques[1],
                                  z=kdl_torques[2])
-            self.publisher.publish(msg)
+            self._publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    jr3_driver = Jr3Driver()
-    rclpy.spin(jr3_driver)
-    rclpy.shutdown()
+
+    try:
+        driver = Jr3Driver()
+
+        try:
+            rclpy.spin(driver)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            driver.close()
+            driver.destroy_node()
+    except RuntimeError:
+        pass
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    finally:
+        if rclpy.ok():
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
