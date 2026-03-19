cc3d0fafd03aa4e9a93560d7bf27af1dd2ba684a
Bartek Łukawski <bwmn.peter@gmail.com>
2026-03-19 14:31:11 +0100

    Review maths

--- jr3_driver/jr3_admittance_controller.py
+++ jr3_driver/jr3_admittance_controller.py
@@ -21,12 +21,18 @@ class Jr3AdmittanceController(Node):
             ParameterDescriptor(description='deadband on force measurements (N)'))
         deadband_torques_param = self.declare_parameter('deadband_torques', 0.0,
             ParameterDescriptor(description='deadband on torque measurements (N*m)'))
+        jr3_trans_x_param = self.declare_parameter('jr3_trans_x', 0.0,
+            ParameterDescriptor(description='x translation from sensor frame to tool frame (m)'))
+        jr3_trans_y_param = self.declare_parameter('jr3_trans_y', 0.0,
+            ParameterDescriptor(description='y translation from sensor frame to tool frame (m)'))
+        jr3_trans_z_param = self.declare_parameter('jr3_trans_z', 0.0,
+            ParameterDescriptor(description='z translation from sensor frame to tool frame (m)'))
         jr3_roll_param = self.declare_parameter('jr3_roll', 0.0,
-            ParameterDescriptor(description='sensor frame roll (rad)'))
+            ParameterDescriptor(description='tool frame roll (rad) wrt. sensor frame'))
         jr3_pitch_param = self.declare_parameter('jr3_pitch', 0.0,
-            ParameterDescriptor(description='sensor frame pitch (rad)'))
+            ParameterDescriptor(description='tool frame pitch (rad) wrt. sensor frame'))
         jr3_yaw_param = self.declare_parameter('jr3_yaw', 0.0,
-            ParameterDescriptor(description='sensor frame yaw (rad)'))
+            ParameterDescriptor(description='tool frame yaw (rad) wrt. sensor frame'))
         gravity_param = self.declare_parameter('gravity', 9.8,
             ParameterDescriptor(description='gravity acceleration (m/s^2)'))
         tool_mass_param = self.declare_parameter('tool_mass', 0.0,
@@ -41,18 +47,26 @@ class Jr3AdmittanceController(Node):
         self.deadband_forces = deadband_forces_param.get_parameter_value().double_value
         self.deadband_torques = deadband_torques_param.get_parameter_value().double_value
 
-        self.R_jr3_tcp = kdl.Rotation.RPY(jr3_roll_param.get_parameter_value().double_value,
-                                          jr3_pitch_param.get_parameter_value().double_value,
-                                          jr3_yaw_param.get_parameter_value().double_value)
+        trans_jr3_tcp = kdl.Vector(jr3_trans_x_param.get_parameter_value().double_value,
+                                   jr3_trans_y_param.get_parameter_value().double_value,
+                                   jr3_trans_z_param.get_parameter_value().double_value)
 
-        self.toolWeight_0 = kdl.Vector(0.0, 0.0, -tool_mass_param.get_parameter_value().double_value * gravity_param.get_parameter_value().double_value)
+        R_jr3_tcp = kdl.Rotation.RPY(jr3_roll_param.get_parameter_value().double_value,
+                                     jr3_pitch_param.get_parameter_value().double_value,
+                                     jr3_yaw_param.get_parameter_value().double_value)
 
-        self.toolCoM_N = kdl.Vector(tool_com_x_param.get_parameter_value().double_value,
-                                    tool_com_y_param.get_parameter_value().double_value,
-                                    tool_com_z_param.get_parameter_value().double_value)
+        self.H_jr3_tcp = kdl.Frame(R_jr3_tcp, trans_jr3_tcp)
+        self.H_tcp_jr3 = self.H_jr3_tcp.Inverse()
 
-        self.wrench = None
-        self.wrench_initial = None
+        self.toolWeight_0 = kdl.Wrench() # initializes .wrench to zero
+        self.toolWeight_0.force = kdl.Vector(0.0, 0.0, -tool_mass_param.get_parameter_value().double_value * gravity_param.get_parameter_value().double_value)
+
+        self.toolCoM_jr3 = kdl.Vector(tool_com_x_param.get_parameter_value().double_value,
+                                      tool_com_y_param.get_parameter_value().double_value,
+                                      tool_com_z_param.get_parameter_value().double_value)
+
+        self.wrench_jr3 = None
+        self.wrench_jr3_initial = None
 
         self.jr3_subscription = self.create_subscription(Wrench, 'jr3', self.jr3_listener_callback, 10)
         self.jr3_subscription # prevent unused variable warning
@@ -64,9 +78,9 @@ class Jr3AdmittanceController(Node):
         self.get_logger().info('JR3 admittance controller is running.')
 
     def jr3_listener_callback(self, msg: Wrench):
-        forces = kdl.Vector(msg.force.x, msg.force.y, msg.force.z)
-        torques = kdl.Vector(msg.torque.x, msg.torque.y, msg.torque.z)
-        self.wrench = kdl.Wrench(forces, torques)
+        force = kdl.Vector(msg.force.x, msg.force.y, msg.force.z)
+        torque = kdl.Vector(msg.torque.x, msg.torque.y, msg.torque.z)
+        self.wrench_jr3 = kdl.Wrench(force, torque)
 
     def egm_command_worker(self):
         with EGM() as egm:
@@ -82,26 +96,28 @@ class Jr3AdmittanceController(Node):
             while self.running:
                 success, state = egm.receive_from_robot(timeout=0.01)
 
-                if success and self.wrench is not None:
-                    R_0_N = kdl.Rotation.Quaternion(state.cartesian.orient.u1, state.cartesian.orient.u2, state.cartesian.orient.u3, state.cartesian.orient.u0)
+                if success and state is not None and state.cartesian is not None and self.wrench_jr3 is not None:
+                    R_0_tcp = kdl.Rotation.Quaternion(state.cartesian.orient.u1, state.cartesian.orient.u2, state.cartesian.orient.u3, state.cartesian.orient.u0)
                     p_0 = kdl.Vector(state.cartesian.pos.x, state.cartesian.pos.y, state.cartesian.pos.z)
-                    H_0_N = kdl.Frame(R_0_N, p_0)
+                    H_0_tcp = kdl.Frame(R_0_tcp, p_0)
 
-                    toolWrench = H_0_N.Inverse() * self.toolWeight_0
-                    toolWrench_N = toolWrench.RefPoint(self.toolCoM_N)
+                    toolWeight_jr3 = H_0_tcp.M.Inverse() * self.toolWeight_0 # tool weight measured on the CoM
+                    toolWeight_jr3 = toolWeight_jr3.RefPoint(-self.toolCoM_jr3) # tool weight measured on the sensor plate
 
-                    wrench = self.wrench - toolWrench_N
+                    wrench_jr3 = self.wrench_jr3 - toolWeight_jr3
 
-                    if self.wrench_initial is None:
-                        self.wrench_initial = wrench
+                    if self.wrench_jr3_initial is None:
+                        self.wrench_jr3_initial = wrench_jr3
 
-                    wrench -= self.wrench_initial
+                    wrench_jr3 -= self.wrench_jr3_initial
 
-                    if wrench.force.Norm() < self.deadband_forces:
-                        wrench.force = kdl.Vector.Zero()
+                    if wrench_jr3.force.Norm() < self.deadband_forces:
+                        wrench_jr3.force = kdl.Vector.Zero()
 
-                    if wrench.torque.Norm() < self.deadband_torques:
-                        wrench.torque = kdl.Vector.Zero()
+                    if wrench_jr3.torque.Norm() < self.deadband_torques:
+                        wrench_jr3.torque = kdl.Vector.Zero()
+
+                    wrench_0 = H_0_tcp * (self.H_tcp_jr3 * wrench_jr3)
 
                     pos = np.array([])
                     orient = np.array([])
